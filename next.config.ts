50f0f9e2ce5481125000221c3cfb3011d47b9236
ouweiya <higogle1@gmail.com>
2025-12-19 13:34:49 +0800

    output

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+    output: 'export',
+    trailingSlash: true,
+
     reactCompiler: true,
     reactStrictMode: true,
+
     basePath: process.env.PAGES_BASE_PATH,
+    images: { unoptimized: true },
 };
 
 export default nextConfig;
